46f765712ee89b9dfd56c6c7a95abeaf92cf2e5e
Graham <graham_howden1@yahoo.co.uk>
2014-05-22 21:33:52 +0100

    add getters and setters

--- src/main/java/com/publicuhc/autoubl/entries/DefaultUBLEntry.java
+++ src/main/java/com/publicuhc/autoubl/entries/DefaultUBLEntry.java
@@ -5,35 +5,96 @@ import java.util.UUID;
 
 public class DefaultUBLEntry implements UBLEntry {
 
-    //TODO this entire class
+    private UUID m_uuid;
+    private String m_reason;
+    private Date m_dateBanned;
+    private String m_lengthOfBan;
+    private Date m_dateUnbanned;
+    private String m_courtroomPost;
 
     @Override
     public UUID getUUID() {
-        return null;
+        return m_uuid;
+    }
+
+    @Override
+    public UBLEntry setUUID(UUID uuid) {
+        m_uuid = uuid;
+        return this;
     }
 
     @Override
     public String getReason() {
-        return null;
+        return m_reason;
+    }
+
+    @Override
+    public UBLEntry setReason(String reason) {
+        m_reason = reason;
+        return this;
     }
 
     @Override
     public Date getDateBanned() {
-        return null;
+        return m_dateBanned;
+    }
+
+    @Override
+    public UBLEntry setDateBanned(Date banned) {
+        m_dateBanned = banned;
+        return this;
     }
 
     @Override
     public String lengthOfBan() {
-        return null;
+        return m_lengthOfBan;
+    }
+
+    @Override
+    public UBLEntry setLengthOfBan(String lengthOfBan) {
+        m_lengthOfBan = lengthOfBan;
+        return this;
     }
 
     @Override
     public Date getExpiryDate() {
-        return null;
+        return m_dateUnbanned;
+    }
+
+    @Override
+    public UBLEntry setExpiryDate(Date expiryDate) {
+        m_dateUnbanned = expiryDate;
+        return this;
     }
 
     @Override
     public String getCourtroomPost() {
-        return null;
+        return m_courtroomPost;
+    }
+
+    @Override
+    public UBLEntry setCourtroomPost(String courtroomPost) {
+        m_courtroomPost = courtroomPost;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("UUID: ")
+                .append(m_uuid.toString())
+                .append(", Reason: ")
+                .append(m_reason)
+                .append(", Date Banned: ")
+                .append(m_dateBanned.toString())
+                .append(", Date Unbanned: ")
+                .append(m_dateUnbanned.toString())
+                .append(", Courtroom Post: ")
+                .append(m_courtroomPost)
+                .append(", Length: ")
+                .append(m_lengthOfBan);
+
+        return builder.toString();
     }
 }
--- src/main/java/com/publicuhc/autoubl/entries/UBLEntry.java
+++ src/main/java/com/publicuhc/autoubl/entries/UBLEntry.java
@@ -11,9 +11,21 @@ public interface UBLEntry {
     UUID getUUID();
 
     /**
+     * @param uuid the UUID to ban
+     * @return this
+     */
+    UBLEntry setUUID(UUID uuid);
+
+    /**
      * @return The reason for the ban
      */
     String getReason();
+
+    /**
+     * @param reason the reason for the ban
+     * @return this
+     */
+    UBLEntry setReason(String reason);
 
     /**
      * @return the date banned
@@ -21,9 +33,21 @@ public interface UBLEntry {
     Date getDateBanned();
 
     /**
+     * @param banned the date banned
+     * @return this
+     */
+    UBLEntry setDateBanned(Date banned);
+
+    /**
      * @return the length of the ban to show
      */
     String lengthOfBan();
+
+    /**
+     * @param lengthOfBan the length of ban to show
+     * @return this
+     */
+    UBLEntry setLengthOfBan(String lengthOfBan);
 
     /**
      * @return the date for unban
@@ -31,8 +55,19 @@ public interface UBLEntry {
     Date getExpiryDate();
 
     /**
+     * @param expiryDate the date for unban
+     * @return this
+     */
+    UBLEntry setExpiryDate(Date expiryDate);
+
+    /**
      * @return the URL to the courtroom post
      */
     String getCourtroomPost();
 
+    /**
+     * @param courtroomPost the URL to the post
+     * @return this
+     */
+    UBLEntry setCourtroomPost(String courtroomPost);
 }
