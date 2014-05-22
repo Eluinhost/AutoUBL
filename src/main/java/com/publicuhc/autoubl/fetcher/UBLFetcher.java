a2019f0665067d9b0482265e10dde379a47d8dbc
Graham <graham_howden1@yahoo.co.uk>
2014-05-22 21:34:16 +0100

    should only use one method

--- src/main/java/com/publicuhc/autoubl/fetcher/UBLFetcher.java
+++ src/main/java/com/publicuhc/autoubl/fetcher/UBLFetcher.java
@@ -7,16 +7,10 @@ import java.util.List;
 public abstract interface UBLFetcher {
 
     /**
-     * Attempt to get the UBL
-     * @throws UBLFetchException if there was an error during the fetch
-     */
-    abstract void fetch() throws UBLFetchException;
-
-    /**
      * Attempt to parse the fetched UBL
      * @return The entries from the fetched UBL
      * @throws UBLParseException if there was an error parsing the data
-     * @throws IllegalStateException if the fetch method hasn't been called yet
+     * @throws UBLFetchException if there was an error fetching the data
      */
-    abstract List<UBLEntry> parse() throws UBLParseException;
+    abstract List<UBLEntry> fetch() throws UBLParseException, UBLFetchException;
 }
