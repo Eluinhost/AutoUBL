82d9159e7ddf1ece6d204e98958cc0446c2b93e5
HowdenG <graham_howden1@yahoo.co.uk>
2014-05-23 17:00:25 +0100

    check for array and then pass each entry to a builder

--- src/main/java/com/publicuhc/autoubl/fetcher/JSONUBLFetcher.java
+++ src/main/java/com/publicuhc/autoubl/fetcher/JSONUBLFetcher.java
@@ -1,13 +1,18 @@
 package com.publicuhc.autoubl.fetcher;
 
+import com.publicuhc.autoubl.entries.JSONUBLEntryBuilder;
 import com.publicuhc.autoubl.entries.UBLEntry;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JSONUBLFetcher implements UBLFetcher {
@@ -44,6 +49,28 @@ public class JSONUBLFetcher implements UBLFetcher {
 
     @Override
     public List<UBLEntry> fetch() throws UBLParseException, UBLFetchException {
-        return null;
+        String jsonString = getJSONString();
+        List<UBLEntry> entries = new ArrayList<UBLEntry>();
+        try {
+            Object rootNode = m_jsonParser.parse(jsonString);
+
+            if(!(rootNode instanceof JSONArray)) {
+                throw new UBLParseException();
+            }
+            JSONArray entrylist = (JSONArray) rootNode;
+
+            for(Object entryNode : entrylist) {
+                if(!(entryNode instanceof JSONObject)) {
+                    throw new UBLParseException();
+                }
+                JSONObject entry = (JSONObject) entryNode;
+                JSONUBLEntryBuilder builder = new JSONUBLEntryBuilder(entry);
+
+                entries.add(builder.build());
+            }
+        } catch (ParseException e) {
+            throw new UBLParseException();
+        }
+        return entries;
     }
 }
